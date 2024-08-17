fa6e99263494047c47cfc398ae1f81ceae4ae286
ndltuan <ndltuan@cmcglobal.vn>
2024-08-18 00:03:21 +0700

    firt commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
