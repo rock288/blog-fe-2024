5378b1952fca021a7e111f49ac58412a220ee905
ndltuan <ndltuan@cmcglobal.vn>
2024-12-24 13:04:10 +0700

    menu

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { MyInfo } from "./my-info"
 
 export function Header() {
   return (
-    <header className="flex justify-between p-2">
+    <header className="flex justify-between py-2">
       {/* <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Home
       </h1> */}
