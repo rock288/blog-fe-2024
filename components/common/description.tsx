6b72c362456ddb78f4db4d1687c29f2da4c7ce12
ndltuan <ndltuan@cmcglobal.vn>
2024-12-25 11:53:56 +0700

    add icon

--- components/common/description.tsx
+++ components/common/description.tsx
@@ -1,17 +1,24 @@
+import { FaLinkedin } from "react-icons/fa"
+
 export function Description() {
   return (
     <section data-tile="info blog" className="pt-4">
       I'm a software engineer working mostly in JavaScript. I enjoy learning and
       seeing a better version of myself each day. Here is where I share my
       knowledge and thoughts throughout my journey, while also finding a way to
-      improve my English. Connect with me on{" "}
-      <a
-        target="_blank"
-        href="https://www.linkedin.com/in/tuan-nguyen-duc-linh-5b8675115/"
-        className="underline"
-      >
-        Linked
-      </a>
+      improve my English.{" "}
+      <span>
+        Connect with me on{" "}
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://www.linkedin.com/in/tuan-nguyen-duc-linh-5b8675115/"
+          className="underline align-text-bottom"
+        >
+          <FaLinkedin className="inline h-4" />
+        </a>
+        .
+      </span>
     </section>
   )
 }
