6da9a97c08c4ef93b836bd4a475f1f7c6bccb442
ndltuan <ndltuan@cmcglobal.vn>
2025-02-23 11:17:40 +0700

    css pb-16

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -6,7 +6,7 @@ export function Layout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="prose mx-auto max-w-prose">
+    <div className="prose mx-auto max-w-prose pb-16">
       <Header />
       {children}
     </div>
