e3a7364945d54a15faf54e2019d04d4d79eafc0f
ndltuan <ndltuan@cmcglobal.vn>
2025-02-23 12:35:36 +0700

    update meta home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,12 @@
-import type { Metadata } from "next"
 import localFont from "next/font/local"
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
-import { URL_HOST } from "@/configs"
 
 const nunito = localFont({
   src: "./fonts/Nunito/Nunito-VariableFont_wght.ttf",
   variable: "--font-nunito",
   weight: "100 800 900",
 })
-
-export const metadata: Metadata = {
-  title: "rock288.com",
-  description: "A software maker working mostly in JavaScript and Go.",
-  keywords: "React, Nodejs, Go, AWS",
-  creator: "rock288",
-  icons: [
-    {
-      url: `${URL_HOST}/favicon.png`,
-    },
-  ],
-  openGraph: {
-    type: "website",
-    url: URL_HOST,
-    title: "rock288.com",
-    description: "A software maker working mostly in JavaScript and Go.",
-    siteName: "rock288.com",
-    images: [
-      {
-        url: `${URL_HOST}/favicon.png`,
-        width: 500,
-        height: 250,
-        alt: "rock288 blog",
-      },
-    ],
-  },
-}
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,34 @@
 import { Layout } from "@/components/common/layout"
+import { URL_HOST } from "@/configs"
 import Home from "@/modules/home/home"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Blog of rock288",
+  description: "A software maker working mostly in JavaScript and Go.",
+  keywords: "React, Nodejs, Go, AWS",
+  creator: "rock288",
+  icons: [
+    {
+      url: `${URL_HOST}/favicon.png`,
+    },
+  ],
+  openGraph: {
+    type: "website",
+    url: URL_HOST,
+    title: "Blog of rock288",
+    description: "A software maker working mostly in JavaScript and Go.",
+    siteName: "rock288.com",
+    images: [
+      {
+        url: `${URL_HOST}/favicon.png`,
+        width: 500,
+        height: 250,
+        alt: "rock288 blog",
+      },
+    ],
+  },
+}
 
 export default function HomePage() {
   return (
